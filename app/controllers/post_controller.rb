bd04717ac0d768fa70a84c61954d6188c3ebb189
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-09 15:08:11 -0500

    added create and show actions

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -7,4 +7,42 @@ class PostController < ApplicationController
   def new
     render 'new.html.erb', locals: { post: Post.new }
   end
-end
+
+  def show
+    blog_entry = Post.find(params[:id])
+    begin
+      respond_to do |format|
+        format.html do
+          render 'show.html.erb', locals: { post: blog_entry }
+        end
+        format.json do
+          render json: blog_entry
+        end
+      end
+    rescue ApplicationController::ParameterMissing => error
+      render json: { error: error.message }, status: 422
+    end
+  end
+
+  def create
+    begin
+      new_post = Post.create(post_params)
+      respond_to do |format|
+        format.html do
+          redirect_to '/posts/#{new_post.id}'
+        end
+        format.json do
+          render json: new_post
+        end
+      end
+    rescue ApplicationController::ParameterMissing => error
+      render json: { error: error.message }, status: 422
+    end
+  end # create
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end # class
